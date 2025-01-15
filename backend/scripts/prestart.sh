d63ea1ae78920298b3a38c181ebf880e75fe2009
0-Chan <kyc4421194@gmail.com>
2025-01-15 10:32:28 +0900

    Add github actions debug code

--- backend/scripts/prestart.sh
+++ backend/scripts/prestart.sh
@@ -4,10 +4,14 @@ set -e
 set -x
 
 # Let the DB start
+python --version
 python app/backend_pre_start.py
 
+echo "1"
 # Run migrations
 alembic upgrade head
+echo "2"
 
 # Create initial data in DB
 python app/initial_data.py
+echo "3"
