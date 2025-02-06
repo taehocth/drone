b4dd767fa445e7d88810e015ad995fa3d3245403
0-Chan <kyc4421194@gmail.com>
2025-02-06 22:35:43 +0900

    feat: geomagnetic-kindex 백엔드 API 추가

--- backend/app/models.py
+++ backend/app/models.py
@@ -112,3 +112,22 @@ class TokenPayload(SQLModel):
 class NewPassword(SQLModel):
     token: str
     new_password: str = Field(min_length=8, max_length=40)
+
+
+class KindexRecent(SQLModel):
+    time: str
+    kp: float
+    kk: float
+
+class Kindex(SQLModel):
+    time: str
+    currentP: float
+    currentK: float
+    max24P: float
+    max24K: float
+    recent: list[KindexRecent]
+
+class KindexResponse(SQLModel):
+    error: bool
+    errorCode: str
+    kindex: Kindex
